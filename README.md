e601ff1960d4b41f7cd7f1cdefd21313e2b60f88
uyeonH <yooyeon0508@gmail.com>
2022-01-27 08:46:43 +0900

    create package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 21Days
+
+[ERD](https://lucid.app/lucidchart/f0d69a1b-fb4e-49f9-9894-ef81ca90f02e/edit?invitationId=inv_45b8e1fe-f576-4caf-967d-7f0276bc70b4)
---
+++ src/main/java/com/drunken/_21days/domain/Item.java
@@ -0,0 +1,10 @@
+package com.drunken._21days.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    Long id;
+}
